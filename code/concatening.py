7969fcb7b7ce05616d397bfbc17a9e3524008118
Eduardo Leffa <eduardovargasleffa@gmail.com>
2025-05-20 21:04:03 +0000

    feat: concatening strings

---
+++ code/concatening.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+data1 = input('Write something: ')
+data2 = input('Write something else: ')
+
+# concatening data
+data = data1 + ' ' + data2
+print('The concatenated data is: ', data)
