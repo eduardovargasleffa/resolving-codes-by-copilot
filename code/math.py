ca7854bf39544b65cb24e34a5d35aa624f686149
Eduardo Leffa <eduardovargasleffa@gmail.com>
2025-05-20 21:07:00 +0000

    feat: math

---
+++ code/math.py
@@ -0,0 +1,10 @@
+# we will request a string and an integer number as input. Then we will have to return the string repeated string the number of times informed.
+
+number1 = int(input('Write a number: '))
+number2 = int(input('Write another number: '))
+
+# # we will return the sum of the two numbers
+def sum_numbers(number1, number2):
+    return number1 + number2
+
+print('The sum of the two numbers is: ', sum_numbers(number1, number2))
